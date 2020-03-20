5f109a9a1287670337c9db94d549b893d52fa185
swas-998 <33576986+swas-998@users.noreply.github.com>
2020-03-20 11:27:48 +0530

    Add files via upload

---
+++ IR.ino
@@ -0,0 +1,30 @@
+/*
+ * Interface InfraRed Sensor Using NodeMCU
+ * By TheCircuit
+ * Red :     5V
+ * Brown: Sensor output
+ * Black : Ground
+*/
+
+int ledPin = 12; // choose pin for the LED
+int inputPin = 0; // choose input pin (for Infrared sensor) 
+int val = 0; // variable for reading the pin status 
+void setup() 
+{ 
+   pinMode(ledPin, OUTPUT); // declare LED as output 
+   pinMode(inputPin, INPUT); // declare Infrared sensor as input
+   Serial.begin(115200);
+} 
+void loop()
+{ 
+   val = digitalRead(inputPin); // read input value
+   Serial.println(val); 
+   /*if (val == HIGH)
+   { // check if the input is HIGH
+      digitalWrite(ledPin, LOW); // turn LED OFF   
+   } 
+   else 
+   { 
+      digitalWrite(ledPin, HIGH); // turn LED ON 
+   } */
+}
